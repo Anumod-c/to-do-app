e64f07c43c23b292b7ad7a24929c15f494325e5f
Anumod-c <anumodc2001@gmail.com>
2024-04-08 16:54:32 +0530

    style changed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,73 +1,93 @@
 import React from 'react'
 import "./Todo.css"
-import { useState,useRef ,useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { IoMdDoneAll } from 'react-icons/io';
 
 
 function Todo() {
-const [todo,setTodo] = useState('');
-const [todos,setTodos]=useState([]);
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
 
-const addTodo=()=>{
-    setTodos([...todos,{list:todo,id:Date.now(),status:false}]);
-    setTodo("")
-    
-}
-const handleSumbmit=(e)=>{
-e.preventDefault();
-}
-const inputRef=useRef("null");
-useEffect(()=>{
-inputRef.current.focus()
-});
-//delteting
-const onDelete=(id)=>{
-setTodos(todos.filter((to)=>{
-  return(
-    to.id!==id
-)
-}))
-}
-//completed
-const onComplete=(id)=>{
-let complete= todos.map((list)=>{
-  if(list.id===id){
-    return({...list,status: !list.status})
+  const addTodo = () => {
+    if (todo !== '') {
+      setTodos([...todos, { list: todo, id: Date.now(), status: false }]);
+      setTodo("")
+    }
+    if (editId) {
+      const editTodo = todos.find((todo) => todo.id === editId);
+      const updateTodo = todos.map((to) => to.id === editTodo.id ? (to = { id: to.id, list: todo }) : (to = { id: to.id, list: to.list })
+      )
+      setTodos(updateTodo
+      )
+      setEditId(0);
+      setTodo("")
+    }
   }
-  return list
-})
-setTodos(complete )
-}
 
+
+  const handleSumbmit = (e) => {
+    e.preventDefault();
+  }
+  const inputRef = useRef("null");
+  useEffect(() => {
+    inputRef.current.focus()
+  });
+  //delteting
+  const onDelete = (id) => {
+    setTodos(todos.filter((to) => {
+      return (
+        to.id !== id
+      )
+    }))
+  }
+  //completed
+  const onComplete = (id) => {
+    let complete = todos.map((list) => {
+      if (list.id === id) {
+        return ({ ...list, status: !list.status })
+      }
+      return list
+    })
+    setTodos(complete)
+  }
+
+  //Edit
+  const onEdit = (id) => {
+    const editTodo = todos.find((to) => to.id === id);
+    setTodo(editTodo.list)
+    setEditId(editTodo.id)
+
+  }
   return (
     <div className='container'>
       <h2>TODO  APP</h2>
       <form className='form-group' onSubmit={handleSumbmit}>
-<input className='input' type="text"value={todo} ref={inputRef} placeholder='Enter Your Todo' onChange={(e)=>{
-setTodo(e.target.value);
-}}/>
-<button onClick={addTodo}>ADD</button>
+        <input className='input' type="text" value={todo} ref={inputRef} placeholder='Enter Your Todo' onChange={(e) => {
+          setTodo(e.target.value);
+        }} />
+        <button onClick={addTodo}>{editId ? "EDIT" : "ADD"}</button>
       </form   >
 
       <div className='list'>
         <ul >
-           {
-            todos.map((to)=>{
-             return  (<li className='list-items'>
-             <div className="list-item-list" id={to.status?"list-item":""}>
-             {to.list}
-             </div>
+          {
+            todos.map((to) => {
+              return (<li className='list-items'>
+                <div className="list-item-list" id={to.status ? "list-item" : ""}>
+                  {to.list}
+                </div>
 
-             <span>
-              <IoMdDoneAll className='list-item-icons' id='complete' title='complete' onClick={()=>onComplete(to.id)}/>
-              <FaEdit className='list-item-icons' id='edit' title='edit' />
-              <MdDelete className='list-item-icons' id='delete' title='deltete' onClick={()=>onDelete(to.id)}/>
-              </span>
+                <span>
+                  <IoMdDoneAll className='list-item-icons' id='complete' title='complete' onClick={() => onComplete(to.id)} />
+                  <FaEdit className='list-item-icons' id='edit' title='edit' onClick={() => { onEdit(to.id) }} />
+                  <MdDelete className='list-item-icons' id='delete' title='deltete' onClick={() => onDelete(to.id)} />
+                </span>
               </li>)
             })
-           }
+          }
         </ul>
       </div>
     </div>
